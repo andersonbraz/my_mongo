fd25c7e3bd7b49e8b22e221c4753c9f2f1c14644
Anderson Braz <braz_anderson@msn.com>
2020-05-13 20:03:10 -0300

    Up Files

--- starter.sh
+++ starter.sh
@@ -1,7 +1,12 @@
 #!/bin/zsh
 
 ## TEST 01
-docker-compose -f "docker-compose.yml" up -d --build
-go run dump.go
+# docker-compose -f "docker-compose.yml" up -d --build
+# go run dump.go
+# docker build -t mongo-client:1.0 . -f Dockerfile
+# docker run -d --name mongo-client -p 8030:8030 mongo-client:1.0
+
+## TEST 02
 docker build -t mongo-client:1.0 . -f Dockerfile
-docker run -d --name mongo-client -p 8030:8030 mongo-client:1.0
+docker-compose -f "docker-compose-inc.yml" up -d --build
+go run dump.go
